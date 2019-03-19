4c4c5afb945f03b747783838181dce77be53b29b
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 17:15:59 -0600

    Added migration for models except bookauthor, typo in book model/test
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates_presence_of :title
-  validates_presence_of :page_count
+  validates_presence_of :pages
   validates_presence_of :year_published
   validates_presence_of :thumbnail
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190319221600) do
+ActiveRecord::Schema.define(version: 20190319230720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "pages"
     t.integer "year_published"
     t.string "thumbnail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "user_name"
+    t.integer "rating"
+    t.string "review_headline"
+    t.string "review_body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
