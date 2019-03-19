7b10ff20ef625703874e51f68b6f4c3977e44ec9
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 17:40:29 -0600

    Fixed typo in book spec
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Book, type: :model do
   describe 'relationships' do
     it {should have_many :reviews}
     it {should belong_to :book_author}
-    it {should have_many(:authors).though :book_author}
+    it {should have_many(:authors).through :book_author}
   end
   describe 'validations' do
     it {should validate_presence_of :title}
