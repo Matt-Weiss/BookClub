237964a1c7e1f7634003b57dfd3e0957e8259dea
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 16:17:32 -0600

    Adds Migration To Remove Author From Book Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190318235038) do
+ActiveRecord::Schema.define(version: 20190319221600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.integer "pages"
     t.integer "year_published"
     t.string "thumbnail"
