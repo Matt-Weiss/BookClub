216cfca214ff5cfee2368577d0a0eed9402de2cc
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 16:44:03 -0600

    Adds validation for review model
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  # describe 'relationships' do
+  #   it {should have_many :reviews}
+  #   it {should belong_to :book_author}
+  #   it {should have_many(:authors).through :book_author}
+  # end
+  describe 'validations' do
+    it {should validate_presence_of :user_name}
+    it {should validate_presence_of :rating}
+    it {should validate_presence_of :review_headline}
+    it {should validate_presence_of :review_body}
+  end
+end
