7cd741b812edfc87be3b5454d7bc406dbd841b8b
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 17:02:01 -0600

    Adds validation in models
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
-
+  validates_presence_of :title
+  validates_presence_of :page_count
+  validates_presence_of :year_published
+  validates_presence_of :thumbnail
 end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates_presence_of :user_name
+  validates_presence_of :rating
+  validates_presence_of :review_headline
+  validates_presence_of :review_body
+end
