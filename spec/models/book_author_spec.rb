a59659ea8b9951834262a9dbfbb6111329420630
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 17:37:09 -0600

    Added correct rspec syntax to book_author spec
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/models/book_author_spec.rb
+++ spec/models/book_author_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BookAuthor, type: :model do
-  it {should have_many :books}
-  it {should have_many :authors}
+  describe 'relationships' do
+    it {should have_many :books}
+    it {should have_many :authors}
+  end 
 end
