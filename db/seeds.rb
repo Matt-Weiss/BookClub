008a52e65b20094670a502929d85e2ab91f1e69f
Matt Weiss <weiss.matt@outlook.com>
2019-03-18 18:35:47 -0600

    added seeds to test heroku initialization
    Co-authored-by: Michael Karnes <44683008+mikekarnes123@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "An Astronaut's Guide to Life on Earth", author: "Chris Hadfield", pages: 284, year_published: 2013, thumbnail: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+Book.create(title: 'CSSuckit', author: 'Matt Weiss, Mike Karnes', pages: 420, year_published: 2019, thumbnail: 'https://i1.wp.com/www.developermemes.com/wp-content/uploads/2014/01/CSS-Sucks-TShirt-Meme.jpg?resize=385%2C232')
