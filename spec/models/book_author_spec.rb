9f47c7f891e93b39940f5e60bd75a9c3dad65d42
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-19 17:36:20 -0600

    Adds relational tests to book_author_spec
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/models/book_author_spec.rb
+++ spec/models/book_author_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe BookAuthor, type: :model do
-
-
+  it {should have_many :books}
+  it {should have_many :authors}
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  
+
+  describe 'relationships' do
+    it {should belong_to :book}
+  end
+
   describe 'validations' do
     it {should validate_presence_of :user_name}
     it {should validate_presence_of :rating}
