cd255a1cfab3919d14ac2a97b9ad69ce45d5861c
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-20 15:27:31 -0600

    Adds book show page authors link to author show page
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'book show page', type: :feature do
     astronaut.reviews.create(user_name: "joe876",rating: 3,review_headline: "BAD :(",review_body: "Css made me want to eat nails nad snails")
 
     visit book_path(astronaut)
-    
+
     expect(page).to have_content(astronaut.reviews.last.user_name)
     expect(page).to have_content(astronaut.reviews.last.rating)
     expect(page).to have_content(astronaut.reviews.last.review_headline)
@@ -36,6 +36,18 @@ RSpec.describe 'book show page', type: :feature do
     expect(page).to have_content(astronaut.reviews[-2].rating)
     expect(page).to have_content(astronaut.reviews[-2].review_headline)
     expect(page).to have_content(astronaut.reviews[-2].review_body)
+  end
+
+  it 'book authors are links to author show page' do
+    astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year_published: 2013, thumbnail: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+    css = Book.create(title: 'CSSucks', pages: 420, year_published: 2019, thumbnail: 'https://i1.wp.com/www.developermemes.com/wp-content/uploads/2014/01/CSS-Sucks-TShirt-Meme.jpg?resize=385%2C232')
+    astronaut.authors.create(name: "Chris Hadfield")
+    astronaut.reviews.create(user_name: "bob123",rating: 2,review_headline: "Good",review_body: "Good book, space good, space fun")
+    astronaut.reviews.create(user_name: "joe876",rating: 3,review_headline: "BAD :(",review_body: "Css made me want to eat nails nad snails")
+
+    visit book_path(astronaut)
+    click_on astronaut.authors.last.name
+    expect(current_path).to eq(author_path(astronaut.authors.last))
   end
 
 end
