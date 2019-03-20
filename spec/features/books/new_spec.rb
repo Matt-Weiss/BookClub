534e0427bf61ff59a1d7c3537e122e63420d7f7f
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-20 16:10:27 -0600

    Adds new book spec
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

---
+++ spec/features/books/new_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'new book page', type: :feature do
+  it 'shows form correctly' do
+    visit new_book_path
+    fill_in 'book[title]', with: 'Lord Of The Rings'
+    fill_in 'book[year_published]', with: 2012
+    fill_in 'book[pages]', with: 315
+    fill_in 'book[thumbnail]', with: 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg'
+    fill_in 'author[name]', with: 'Tolken'
+    click_button 'Create Book'
+  end
+
+
+
+end
