5d3ec85230b19db4ff483846bf12904052011d71
Matt Weiss <weiss.matt@outlook.com>
2019-03-20 18:24:20 -0600

    Clean up new book input
    Co-authored-by: Michael Karnes <44683008+mikekarnes123@users.noreply.github.com>

--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'new book page', type: :feature do
       fill_in 'book[year_published]', with: 2012
       fill_in 'book[pages]', with: 315
       fill_in 'book[thumbnail]', with: 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg'
-      fill_in 'book[authors]', with: 'Tolken'
+      fill_in 'author_names', with: 'Tolkien'
       click_button 'Create Book'
       book = Book.last
       expect(current_path).to eq(book_path(book))
