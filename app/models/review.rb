a12efe271e16bc68b4b274e7b51b4e00c71297eb
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-22 22:25:40 -0600

    Switches rating average method to review class method, NEED TO FIX ROUNDING BUG

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,5 +8,8 @@ class Review < ApplicationRecord
   def self.no_duplicate_user_names(review_params)
     create_with(review_body: review_params[:review_body], review_headline: review_params[:review_headline], rating: review_params[:rating]).find_or_create_by(user_name: review_params[:user_name])
   end
-
+  def self.overall_rating
+    #NEED TO SOLVE ROUNDING ISSUE
+    average(:rating)
+  end
 end
