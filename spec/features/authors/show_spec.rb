f91ec8c067bd53c1c1b547aaf1d326fe0dc6179d
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-03-25 18:00:30 -0600

    Backfills 2 views with testing

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -28,9 +28,31 @@ RSpec.describe 'at the author show page', type: :feature do
     author_2 = book.authors.create(name: 'Matt Weiss')
 
     visit author_path(author_1)
-    
+
     expect(page).to_not have_link('Tolkien')
     expect(page).to have_link('Matt Weiss')
   end
 
+  it 'should display proper stats' do
+    author = Author.create(name: 'Tolkien')
+    book_1 = author.books.create(title: 'Lord Of The Rings', year_published: 2012, pages: 315, thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg')
+    book_2 = author.books.create(title: 'Lord Of The Rings2', year_published: 2013, pages: 456, thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg')
+    book_1.reviews.create(review_headline: "top review", review_body: "im the best review", rating: 5, user_name: "blah")
+    book_1.reviews.create(review_headline: "not the top review", review_body: "im not the top review", rating: 2, user_name: "bob")
+    book_2.reviews.create(review_headline: "top review", review_body: "im the best review", rating: 5, user_name: "bob")
+    book_2.reviews.create(review_headline: "not the top review", review_body: "im not the top review", rating: 2, user_name: "blah")
+
+    visit author_path(author)
+    expect(page).to have_content('Lord Of The Rings')
+    expect(page).to have_content('2012')
+    expect(page).to have_content('315')
+    expect(page).to have_content('5')
+    expect(page).to have_content('blah')
+    expect(page).to have_xpath("/html/body/ul/img[1]")
+    expect(page).to have_content('Lord Of The Rings2')
+    expect(page).to have_content('2013')
+    expect(page).to have_content('5')
+    expect(page).to have_content('bob')
+    expect(page).to have_xpath("/html/body/ul/img[2]")
+  end
 end
